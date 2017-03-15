4baa2d5424fefce157251242c13a6a1a481e884d
Gabi Giladov <gabi.giladov@gmail.com>
2017-03-15 03:32:18 +0200

    Karma tests

--- final project git-client/src/app/tests/aceTry.spec.ts
+++ final project git-client/src/app/tests/aceTry.spec.ts
@@ -4,29 +4,27 @@ describe('Ace Editor Component', () => {
 
   let comp:    MyComponent = new MyComponent();
 
-
-
-    it('Change Read Only Mode', function() {
+    it('Change Read Only Mode by default', function() {
         expect(comp.readonly).toBe(false);
     });
 
-    it('Change Read Only Mode', function() {
+    it('Change Read Only Mode to true', function() {
       comp.changeReadOnlyModeToTrue();
       expect(comp.readonly).toBe(true);
     });
 
-    it('Change Read Only Mode', function() {
+    it('Change Read Only Mode to false', function() {
       comp.changeReadOnlyModeToFalse();
       expect(comp.readonly).toBe(false);
     });
 
-    it('Change Read Only Mode', function() {
-      comp.changeText('Test Test Code Monkey');
-      expect(comp.text).toBe('Test Test Code Monkey');
+    it('Change Topic', function() {
+      comp.changeText('Test Code Monkey');
+      expect(comp.text).toBe('Test Code Monkey');
     });
 
-    it('Change Read Only Mode', function() {
-      comp.onChange('Test Test Code Monkey');
+    it('On Change', function() {
+      comp.onChange('Test Code Monkey');
     });
 
 });
---
+++ final project git-client/src/app/tests/app.stage.component.spec.ts
@@ -0,0 +1,50 @@
+import {stageComponent} from '../app.stage.component';
+import {StagePageService} from '../stage.component.service'
+import {Configuration} from '../configuration';
+import {Response, RequestOptions, Headers} from "@angular/http";
+
+describe('Stage Component', () => {
+  let headers = new Headers({ 'Content-Type': 'application/json; charset=utf-8',
+    'Accept': '*/*'});
+  let options = new RequestOptions({ headers: headers });
+  let stage_id: string = '1';
+  let conf: Configuration = new Configuration();
+  let stage_service: StagePageService = new StagePageService(conf.serverURL);
+  let stage:  stageComponent = new stageComponent(stage_service, null, null);
+
+   it('Chatbot is hidden by default', function() {
+     expect(stage.chatbotIsOn).toBe(false);
+   });
+
+  it('Hints are hidden by default', function() {
+    expect(stage._isCollapsedContent).toBe(true);
+  });
+
+
+  it('Show Hints', function() {
+    stage.collapseHints();
+    expect(stage._isCollapsedContent).toBe(false);
+  });
+
+  // it('Ace editor permission after successful compilation', function() {
+  //   stage.title = 'app works!';
+  //   stage.changeEditorEdit();
+  //   expect(stage.editor.readonly).toBe(true);
+  // });
+  //
+  // it('Ace editor permission after failed compilation', function() {
+  //   stage.title = 'failed compilation';
+  //   stage.changeEditorEdit();
+  //   expect(stage.editor.readonly).toBe(false);
+  // });
+
+  it('Page Theme is dark by default', function() {
+    expect(stage.isDarkTheme).toBe(true);
+  });
+
+  // it('Change Theme', function() {
+  //   stage.changeToDarkTheme();
+  //   expect(stage.isDarkTheme).toBe(false);
+  // });
+});
+
