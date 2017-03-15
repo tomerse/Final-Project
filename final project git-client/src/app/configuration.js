6cf0c5b70b367b28c033eebbc2c2e00e59b91535
Gabi Giladov <gabi.giladov@gmail.com>
2017-03-15 20:48:37 +0200

    tests

---
+++ final project git-client/src/app/configuration.js
@@ -0,0 +1,10 @@
+"use strict";
+var Configuration = (function () {
+    function Configuration() {
+        this.serverURL = 'http://finalprojectcm.herokuapp.com/';
+        this.currLang = 'python';
+        this.courseName = 'chatbot';
+    }
+    return Configuration;
+}());
+exports.Configuration = Configuration;
