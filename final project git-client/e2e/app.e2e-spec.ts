641a0337e9bb0cf85f3a08411108f11c9f2bddf1
Gabi Giladov <gabi.giladov@gmail.com>
2017-04-18 01:55:14 +0300

    e2e more tests

--- final project git-client/e2e/app.e2e-spec.ts
+++ final project git-client/e2e/app.e2e-spec.ts
@@ -6,8 +6,20 @@ describe('CodeMonkey App', function() {
   beforeEach(() => {
     page = new MainPage();
   });
+
+  it('should display correct title', function () {
+    page.navigateTo();
+    expect(page.getTitle()).toEqual('Monkey');
+  });
+
   it('should display heading', () => {
     page.navigateTo();
     expect(page.getHeadingText()).toEqual('Monkey-Bot academy');
   });
+
+  it('navigate to course page', () => {
+    page.navigateTo();
+    page.getCourseButton().click();
+  });
+
 });
