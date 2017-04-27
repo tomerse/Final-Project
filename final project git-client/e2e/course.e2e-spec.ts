09dbe0c8c51c06d3c7d4cbe2020c3632104e0079
Gabi Giladov <gabi.giladov@gmail.com>
2017-04-27 18:34:50 +0300

    e2e more

--- final project git-client/e2e/course.e2e-spec.ts
+++ final project git-client/e2e/course.e2e-spec.ts
@@ -20,4 +20,41 @@ describe('Python Course', function() {
     expect(page.getCourseTopic()).toEqual('Exercise 1');
     page.enableSynch();
   });
+
+  it('Change Theme button text', function () {
+    page.ignoreSynch();
+    page.navigateTo();
+    expect(page.changeThemeButton().getText()).toEqual('TOGGLE DARK THEME');
+    page.enableSynch();
+  });
+
+  it('Change Theme', function () {
+    page.ignoreSynch();
+    page.navigateTo();
+    var i;
+    for (i = 0; i < 10; i++) {
+      page.changeThemeButton().click();
+    }
+    expect(page.changeThemeButton().getText()).toEqual('TOGGLE DARK THEME');
+    page.enableSynch();
+  });
+
+  it('Submit init code should fail', function () {
+    page.ignoreSynch();
+    page.navigateTo();
+    expect(page.getSubmitCodeButton().getText()).toEqual('Submit your code!');
+    page.getSubmitCodeButton().click();
+    expect(page.getAfterSubmitCodePopup()).toBeTruthy();
+    page.enableSynch();
+  });
+
+  it('Trying go to the next stage', function () {
+    page.ignoreSynch();
+    page.navigateTo();
+    expect(page.getNextStageButton().getText()).toEqual('Start next lesson');
+    page.getNextStageButton().click();
+    expect(page.getCourseTopic()).toEqual('Exercise 1');
+    page.enableSynch();
+  });
+
 });
--- final project git-client/e2e/course.po.ts
+++ final project git-client/e2e/course.po.ts
@@ -23,4 +23,41 @@ export class CoursePage {
   getCourseTopic() {
     return this.getCourse().element(by.css('h3')).getText();
   }
+
+  changeThemeButton() {
+    return element(by.id('changeTheme'));
+  }
+
+  chatbotComponent() {
+    return element(by.css('.chat-window-container'));
+  }
+
+  aceEditorComponent() {
+    return element(by.tagName('aceeditor'));
+  }
+
+  getSubmitCodeButton() {
+    return element(by.id('submitCodeButton'));
+  }
+
+  getCoursesListButton() {
+    return element(by.id('courseList'));
+  }
+
+  goToLevel(LevelNum:number) {
+      element.all(by.css('.md-list-item')).get(LevelNum).element(by.css('a')).click();
+  }
+
+  getNextStageButton() {
+    return element(by.id('nextStageButton'));
+  }
+
+  getAfterSubmitCodePopup() {
+    return element(by.css('.cdk-overlay-pane'));
+  }
+
+  getSubmitCodeResult() {
+    return this.getAfterSubmitCodePopup().element(by.css('h1'));
+  }
+
 }
